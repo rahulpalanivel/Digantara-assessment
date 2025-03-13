64340e997cb81aa5cade256c55ef87ae2255839e
rahulpalanivel <palanivelrahul45@gmail.com>
2025-03-14 00:47:09 +0530

    chore: added binary search and quick sort functions

--- services/algorithmService.js
+++ services/algorithmService.js
@@ -1,9 +1,41 @@
 const binarySearch = (array, number) => {
-  return 1;
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (array[mid] === number) {
+      return mid;
+    }
+
+    if (number < array[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
 };
 
 const quickSort = (array) => {
-  return 2;
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array[0];
+  let leftArray = [];
+  let rightArray = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      leftArray.push(array[i]);
+    } else {
+      rightArray.push(array[i]);
+    }
+  }
+
+  return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
 };
 
 const breadthFirstSearch = () => {
